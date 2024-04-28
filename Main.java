09f9e5e6d21b11bbc92b0c2eb8afe5c000df9e5e
Fajar Lintang Gumilang <fajar2943@gmail.com>
2024-04-29 00:18:52 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+	public static void main(String[] arg){
+		int jam = 8; // Ganti dengan jam sekarang
+
+        if(jam > 7) {
+            System.out.println("Anda Terlambat");
+        } else {
+            System.out.println("Anda Tepat waktu");
+        }
+	}
+}
